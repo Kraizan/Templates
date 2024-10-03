1ae7e038245977470ee2a275505f297d42a1ef11
Vanshaj Bhatnagar <63310123+Kraizan@users.noreply.github.com>
2024-10-04 03:07:46 +0530

    Updated SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,9 +1,39 @@
 class SegTree {
 	vector<int> seg;
+	int N;
+	
+	void _update(int ind, int low, int high, int i, int val){
+		if(low == high){
+			seg[ind] = val;
+			return;
+		}
+		
+		int mid = (low + high) / 2;
+		if(i <= mid) _update(2*ind+1, low, mid, i, val);
+		else _update(2*ind+2, mid+1, high, i, val);
+		
+		seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+	}
+	
+	int _query(int ind, int low, int high, int l, int r){
+		if(low > r || high < l){
+			return 0;
+		}
+		else if(low >= l && high <= r){
+			return seg[ind];
+		}
+		else{
+			int mid = (low + high)/2;
+			return _query(2*ind+1, low, mid, l, r) + _query(2*ind+2, mid+1, high, l, r);
+		}
+	}
+	
 public:
 	SegTree(int n){
+		N = n;
 		seg.resize(4*n + 1);
 	}
+	
 	void build(int ind, int low, int high, vector<int> &arr){
 		if(low == high){
 			seg[ind] = arr[low];
@@ -16,28 +46,12 @@ public:
 		
 		seg[ind] = seg[2*ind+1] + seg[2*ind+2];
 	}
-	void update(int ind, int low, int high, int i, int val){
-		if(low == high){
-			seg[ind] = val;
-			return;
-		}
-		
-		int mid = (low + high) / 2;
-		if(i <= mid) update(2*ind+1, low, mid, i, val);
-		else update(2*ind+2, mid+1, high, i, val);
-		
-		seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+	
+	void update(int ind, int val){
+		_update(0, 0, N-1, ind, val);
 	}
-	int query(int ind, int low, int high, int l, int r){
-		if(low > r || high < l){
-			return 0;
-		}
-		else if(low >= l && high <= r){
-			return seg[ind];
-		}
-		else{
-			int mid = (low + high)/2;
-			return query(2*ind+1, low, mid, l, r) + query(2*ind+2, mid+1, high, l, r);
-		}
+	
+	int query(int l, int r){
+		return _query(0, 0, N-1, l, r);
 	}
 };
