e629f5ab5095915063a6d7b25c2a25a539ee0cea
Vanshaj Bhatnagar <63310123+Kraizan@users.noreply.github.com>
2024-10-07 05:18:21 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,6 +1,5 @@
 class DSU {
-    vector<int> par;
-    vector<int> rank;
+    vector<int> par, rank, size;
 
     int findPar(int x){
         if(par[x] == x) return x;
@@ -21,10 +20,25 @@ class DSU {
         return true;
     }
 
+    bool unionBySize(int x, int y){
+        if(isConnected(x,y)) return false;
+        int upx = par[x], upy = par[y];
+        if(size[upx] > size[upy]){
+            par[upy] = upx;
+            size[upx] += size[upy];
+        }
+        else{
+            par[upx] = upy;
+            size[upy] += size[upx];
+        }
+        return true;
+    }
+
 public:
     DSU(int n){
         par.resize(n);
         rank.resize(n);
+        size.resize(n, 1);
         for(int i=0; i<n; i++){
             par[i] = i;
             rank[i] = i;
@@ -35,7 +49,12 @@ public:
         return findPar(x) == findPar(y);
     }
 
-    bool connect(int x, int y){
-        return unionByRank(x,y);
+    bool connect(int x, int y, bool bySize = false){
+        return bySize ? unionBySize(x,y) : unionByRank(x,y);
+    }
+
+    int getSize(int x){
+        int upx = findPar(x);
+        return size[upx];
     }
 };
